b06b3a40dfbee4a56098d7ad6bd2925926ac47c6
mike <mikedutuandu@gmail.com>
2024-10-21 11:30:58 +0700

    update doc

--- src/modules/v1/quiz/response/quiz-detail.response.ts
+++ src/modules/v1/quiz/response/quiz-detail.response.ts
@@ -24,6 +24,47 @@ export class QuizDetailResponse {
   timeLimit!: number;
 
   @Expose()
-  @ApiProperty()
+  @ApiProperty({
+    description: 'Array of quiz questions',
+    type: [QuizQuestion],
+    example: [
+      {
+        id: 1,
+        questionText: 'What is the capital of France?',
+        quizQuestionOptions: [
+          {
+            id: 1,
+            optionText: 'Paris',
+          },
+          {
+            id: 2,
+            optionText: 'London',
+          },
+          {
+            id: 3,
+            optionText: 'Berlin',
+          },
+        ],
+      },
+      {
+        id: 2,
+        questionText: 'Which planet is known as the Red Planet?',
+        quizQuestionOptions: [
+          {
+            id: 4,
+            optionText: 'Mars',
+          },
+          {
+            id: 5,
+            optionText: 'Venus',
+          },
+          {
+            id: 6,
+            optionText: 'Jupiter',
+          },
+        ],
+      },
+    ],
+  })
   readonly quizQuestions: QuizQuestion[];
 }
